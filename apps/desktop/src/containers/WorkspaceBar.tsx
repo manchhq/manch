cbf639cb0e2399cfa33713b1a27a393ffaa069a6
Kunjan Dalal <kunjee17@gmail.com>
2026-06-30 12:51:49 +0530

    feat(desktop): wire WorkspaceSwitcher to workspaces query + active atom

--- apps/desktop/src/containers/WorkspaceBar.tsx
+++ apps/desktop/src/containers/WorkspaceBar.tsx
@@ -1,7 +1,31 @@
+// WorkspaceBar.tsx
+import { useEffect } from "react";
+import { useAtom } from "jotai";
 import { WorkspaceSwitcher } from "@manch/ui";
+import { activeWorkspaceIdAtom } from "../store/atoms";
+import { useWorkspaces, useCreateWorkspace } from "../data/queries";
 
 export default function WorkspaceBar() {
+  const [activeId, setActiveId] = useAtom(activeWorkspaceIdAtom);
+  const workspaces = useWorkspaces();
+  const create = useCreateWorkspace();
+
+  const list = workspaces.data ?? [];
+  useEffect(() => {
+    if (!activeId && list.length > 0) setActiveId(list[0].id);
+  }, [activeId, list, setActiveId]);
+
   return (
-    <WorkspaceSwitcher workspaces={[]} activeId={null} onSelect={() => {}} onCreate={() => {}} />
+    <WorkspaceSwitcher
+      workspaces={list.map((w) => ({ id: w.id, name: w.name }))}
+      activeId={activeId}
+      onSelect={setActiveId}
+      onCreate={() =>
+        create.mutate(
+          { name: "New workspace", description: "" },
+          { onSuccess: (w) => setActiveId(w.id) },
+        )
+      }
+    />
   );
 }
